9af114899fd131b276c6f05da6f93292ea623b8e
liangzhaozeng <liangzhao.zeng@gmail.com>
2014-04-08 16:17:02 -0400

    mergesort

--- main/java/algorithm/basic/MergeSort.java
+++ main/java/algorithm/basic/MergeSort.java
@@ -1,5 +1,7 @@
 package algorithm.basic;
 
+import java.util.Arrays;
+
 public class MergeSort extends Sort {
 
   @Override
@@ -42,4 +44,56 @@ public class MergeSort extends Sort {
     }
   }
 
+  static void mergeSort(int[] A) {
+      if (A.length > 1) {
+          int q = A.length/2;
+          int[] leftArray = Arrays.copyOfRange(A, 0, q);
+          int[] rightArray = Arrays.copyOfRange(A,q+1,A.length);
+          mergeSort(leftArray);
+          mergeSort(rightArray);
+          A = merge(leftArray,rightArray);
+      }
+  }
+
+  static int[] merge(int[] l, int[] r) {
+      int totElem = l.length + r.length;
+      int[] a = new int[totElem];
+      int i,li,ri;
+      i = li = ri = 0;
+      while ( i < totElem) {
+          if ((li < l.length) && (ri<r.length)) {
+              if (l[li] < r[ri]) {
+                  a[i] = l[li];
+                  i++;
+                  li++;
+              }
+              else {
+                  a[i] = r[ri];
+                  i++;
+                  ri++;
+              }
+          }
+          else {
+              if (li >= l.length) {
+                  while (ri < r.length) {
+                      a[i] = r[ri];
+                      i++;
+                      ri++;
+                  }
+              }
+              if (ri >= r.length) {
+                  while (li < l.length) {
+                      a[i] = l[li];
+                      li++;
+                      i++;
+                  }
+              }
+          }
+      }
+      return a;
+
+  }
+
+  
+  
 }
