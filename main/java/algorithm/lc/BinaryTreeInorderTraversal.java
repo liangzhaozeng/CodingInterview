745ed3433a4f56952f78c567bebcdcf4587bf482
liangzhaozeng <liangzhao.zeng@gmail.com>
2014-04-09 16:14:54 -0400

    qq

--- main/java/algorithm/lc/BinaryTreeInorderTraversal.java
+++ main/java/algorithm/lc/BinaryTreeInorderTraversal.java
@@ -8,11 +8,7 @@ import java.util.Stack;
  * 
  * For example: Given binary tree {1,#,2,3},
  * 
- *   1 
- *    \ 
- *     2 
- *    / 
- *   3 
+ * 1 \ 2 / 3
  * 
  * return [1,3,2].
  * 
@@ -20,41 +16,74 @@ import java.util.Stack;
  * 
  */
 public class BinaryTreeInorderTraversal {
-  
-  public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
 
-    TreeNode(int x) {
-      val = x;
-    }
-  }
-  
-  // O(n) space, O(n) time
-  public class Solution {
-    public ArrayList<Integer> inorderTraversal(TreeNode root) {
-          // Note: The Solution object is instantiated only once and is reused by each test case.
-      TreeNode cur = root;
-      Stack<TreeNode> stack = new Stack<TreeNode>();
-      ArrayList<Integer> res = new ArrayList<Integer>();
-      
-      while (cur != null || !stack.isEmpty()) {
-        // push all left 
-        while (cur != null) {
-          stack.push(cur);
-          cur = cur.left;
-        }
-        
-        TreeNode next = stack.pop();
-        res.add(next.val);
-        
-        // push right
-        cur = next.right;
-      }
-      
-      return res;
-    }
-  }
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	// O(n) space, O(n) time
+	public class Solution {
+
+		public ArrayList<Integer> inorderTraversal2(TreeNode root) {
+			ArrayList<Integer> result = new ArrayList<Integer>();
+			TreeNode current, pre;
+
+			if (root == null)
+				return result;
+
+			current = root;
+			while (current != null) {
+				if (current.left == null) {
+					result.add(current.val);
+					current = current.right;
+				} else {
+					/* Find the inorder predecessor of current */
+					pre = current.left;
+					while (pre.right != null && pre.right != current)
+						pre = pre.right; /* Make current as right child of its inorder predecessor */
+					if (pre.right == null)  {
+						pre.right = current;
+						current = current.left;
+					}	else {                     // Revert the changes made in if part to restore the
+						pre.right = null;          // original tree i.e., fix the right child of predecssor
+						result.add(current.val);
+						current = current.right;
+					} /* End of if condition pre->right == NULL */
+				} /* End of if condition current->left == NULL */
+			} /* End of while */
+
+			return result;
+		}
+
+		public ArrayList<Integer> inorderTraversal(TreeNode root) {
+			// Note: The Solution object is instantiated only once and is reused
+			// by each test case.
+			TreeNode cur = root;
+			Stack<TreeNode> stack = new Stack<TreeNode>();
+			ArrayList<Integer> res = new ArrayList<Integer>();
+
+			while (cur != null || !stack.isEmpty()) {
+				// push all left
+				while (cur != null) {
+					stack.push(cur);
+					cur = cur.left;
+				}
+
+				TreeNode next = stack.pop();
+				res.add(next.val);
+
+				// push right
+				cur = next.right;
+			}
+
+			return res;
+		}
+	}
 
 }
--- main/java/algorithm/lc/RestoreIPAddress.java
+++ main/java/algorithm/lc/RestoreIPAddress.java
@@ -14,50 +14,87 @@ import java.util.ArrayList;
 // O(3^4) space, O(3^4) time, actually both are constant
 public class RestoreIPAddress {
 
-  public class Solution {
-    // use three levels of loop
-    public ArrayList<String> restoreIpAddresses(String s) {
-      // Start typing your Java solution below
-      // DO NOT write main() function
-      ArrayList<String> res = new ArrayList<String>();
-      for (int first = 1; first < s.length(); ++first) {
-        if (!isValid(s, 0, first))
-          break;
+	public class Solution {
 
-        for (int second = first + 1; second < s.length(); ++second) {
-          if (!isValid(s, first, second))
-            break;
+		public ArrayList<String> restoreIpAddresses1(String s) {
+			ArrayList<String> res = restoreIPAddresses(s, 4);
+			if (res == null)
+				res = new ArrayList<String>();
+			return res;
+		}
 
-          for (int third = second + 1; third < s.length(); ++third) {
-            if (!isValid(s, second, third))
-              break;
+		public ArrayList<String> restoreIPAddresses(String s, int k) {
+			assert (k <= 4 && k >= 1);
+			if (s == null || s.length() < k || s.length() > 3 * k)
+				return null;
+			ArrayList<String> res = new ArrayList<String>();
 
-            if (isValid(s, third, s.length())) {
-              // IP is valid
-              StringBuilder sb = new StringBuilder();
-              sb.append(s.substring(0, first)).append('.');
-              sb.append(s.substring(first, second)).append('.');
-              sb.append(s.substring(second, third)).append('.');
-              sb.append(s.substring(third));
-              res.add(sb.toString());
-            }
-          }
+			for (int i = 0; i < Math.min(s.length(), 3); i++) {
+				String num = s.substring(0, i + 1);
+				if ((i == 0 || num.charAt(0) > '0')
+						&& Integer.parseInt(num) <= 255) {
+					if (k == 1) {
+						if (i == s.length() - 1)
+							res.add(num);
+					} else {
+						ArrayList<String> remain = restoreIPAddresses(
+								s.substring(i + 1), k - 1);
+						if (remain != null) {
+							for (String r : remain) {
+								String temp = num + '.' + r;
+								res.add(temp);
+							}
+						}
+					}
+				} else
+					break;
+			}
+			return res;
+		}
 
-        }
-      }
-      return res;
-    }
+		// use three levels of loop
+		public ArrayList<String> restoreIpAddresses(String s) {
+			// Start typing your Java solution below
+			// DO NOT write main() function
+			ArrayList<String> res = new ArrayList<String>();
+			for (int first = 1; first < s.length(); ++first) {
+				if (!isValid(s, 0, first))
+					break;
 
-    private boolean isValid(String s, int start, int end) {
-      if (end - start > 3 || (end - start > 1 && s.charAt(start) == '0')) {
-        return false;
-      }
-      int num = Integer.parseInt(s.substring(start, end));
-      if (num < 256 && num >= 0) {
-        return true;
-      }
-      return false;
-    }
-  }
+				for (int second = first + 1; second < s.length(); ++second) {
+					if (!isValid(s, first, second))
+						break;
+
+					for (int third = second + 1; third < s.length(); ++third) {
+						if (!isValid(s, second, third))
+							break;
+
+						if (isValid(s, third, s.length())) {
+							// IP is valid
+							StringBuilder sb = new StringBuilder();
+							sb.append(s.substring(0, first)).append('.');
+							sb.append(s.substring(first, second)).append('.');
+							sb.append(s.substring(second, third)).append('.');
+							sb.append(s.substring(third));
+							res.add(sb.toString());
+						}
+					}
+
+				}
+			}
+			return res;
+		}
+
+		private boolean isValid(String s, int start, int end) {
+			if (end - start > 3 || (end - start > 1 && s.charAt(start) == '0')) {
+				return false;
+			}
+			int num = Integer.parseInt(s.substring(start, end));
+			if (num < 256 && num >= 0) {
+				return true;
+			}
+			return false;
+		}
+	}
 
 }
